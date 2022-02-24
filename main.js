56a0531f3e05a45fd349baadc43cf287c8efd820
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-02-24 04:18:54 +0100

    completed the css

---
+++ main.js
@@ -0,0 +1,38 @@
+const asta=document.getElementsByClassName('asta')
+asta.onclick = astaAnime
+
+const eren=document.getElementsByClassName('erenYegger')
+eren.onclick = erenYeggerAnime;
+document.getElementsByClassName('shinra').onclick = shinraAnime
+document.getElementsByClassName('deku').onclick = dekuAnime
+document.getElementsByClassName('tanjiro').onclick = tanjiroAnime
+document.getElementsByClassName('Naruto').onclick = NarutoAnime
+
+function astaAnime(){
+document.getElementsByClassName('displayOne').style.backgroundImage = 'url("./images/asta_black_clover.jpg")'
+
+document.getElementsByClassName('displayOne').style.backgroundRepeat=noRepeat;
+
+}
+
+
+function erenYeggerAnime(){
+// function goes here
+}
+
+
+function shinraAnime(){
+// function goes here
+}
+
+function dekuAnime(){
+// function goes here
+}
+
+function tanjiroAnime(){
+// function goes here
+}
+
+function NarutoAnime(){
+// function goes here
+}
