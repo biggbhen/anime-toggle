70569337615c09ae83d197a8538f73bfbbab3b66
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-02-25 23:50:51 +0100

    completed

--- main.js
+++ main.js
@@ -1,39 +1,85 @@
-document.querySelector('.asta').addEventListener('click' , asta)
+document.querySelector('.asta').addEventListener('click' , asta);
+document.querySelector('.erenYegger').addEventListener('click' , erenYegger);
+document.querySelector('.shinra').addEventListener('click' , shinra);
+document.querySelector('.deku').addEventListener('click' , deku);
+document.querySelector('.tanjiro').addEventListener('click' , tanjiro);
+document.querySelector('.Naruto').addEventListener('click' , Naruto);
+document.querySelector('button').addEventListener('click' , button)
 
-
-// const eren=document.getElementsByClassName('erenYegger')
-// eren.onclick = erenYeggerAnime;
-// document.getElementsByClassName('shinra').onclick = shinraAnime
-// document.getElementsByClassName('deku').onclick = dekuAnime
-// document.getElementsByClassName('tanjiro').onclick = tanjiroAnime
-// document.getElementsByClassName('Naruto').onclick = NarutoAnime
 
 function asta(){
-document.querySelector('.displayOne').style.backgroundImage = 'url("images/black cloverGif.gif")'
+ let character=document.querySelector('.nameOfCharacter1')
+ let anime= document.querySelector('.displayOne').style
+anime.backgroundImage = 'url("images/black cloverGif.gif")'
+anime.backgroundRepeat='no-repeat'
+anime.backgroundPosition= 'center'
+anime.backgroundSize='cover'
+character.style.color='red'
+character.innerText='asta'
 
-document.querySelector('.displayOne').style.backgroundRepeat='no-Repeat'
-document.querySelector('.displayOne').style.backgroundSize='cover'
  }
  
 
 
-// function erenYeggerAnime(){
-// // function goes here
-// }
-
-
-function shinraAnime(){
-// function goes here
+function erenYegger(){
+  let character=document.querySelector('.nameOfCharacter1')
+let anime= document.querySelector('.displayOne').style
+anime.backgroundImage='url("images/eren-titan-attack-on-titanGif.gif")'
+anime.backgroundPosition='center'
+anime.backgroundSize='cover'
+character.style.color='orange'
+character.innerText='erenYegger'
 }
 
-function dekuAnime(){
-// function goes here
+
+function shinra(){
+   let character=document.querySelector('.nameOfCharacter1')
+let anime= document.querySelector('.displayOne').style
+anime.backgroundImage='url("images/shinraFireForceGif.gif")'
+anime.backgroundPosition='center'
+anime.backgroundSize='cover'
+character.style.color='white'
+character.innerText='Shinra'
 }
 
-function tanjiroAnime(){
-// function goes here
+function deku(){
+ let character=document.querySelector('.nameOfCharacter2')
+let anime= document.querySelector('.displayTwo').style
+anime.backgroundImage='url("images/my-hero-academia-icegif.gif")'
+anime.backgroundPosition='center'
+anime.backgroundSize='cover'
+character.style.color='white'
+character.innerText='Deku'
 }
 
-function NarutoAnime(){
-// function goes here
+function tanjiro(){
+ let character=document.querySelector('.nameOfCharacter2')
+let anime= document.querySelector('.displayTwo').style
+anime.backgroundImage='url("images/demon-slayer-gif.gif")'
+anime.backgroundPosition='center'
+anime.backgroundSize='cover'
+character.style.color='red'
+character.innerText='tanjiro'
+}
+
+
+function Naruto(){
+ let character=document.querySelector('.nameOfCharacter2')
+let anime= document.querySelector('.displayTwo').style
+anime.backgroundImage='url("images/narutoGif.gif")'
+anime.backgroundPosition='center'
+anime.backgroundSize='cover'
+character.style.color='orange'
+character.innerText='Naruto'
+}
+
+function button(){
+ let fight= document.querySelector('.vsFight')
+ let fire=  document.querySelector('body').style
+ fire.backgroundImage='url("images/bXdxUz.gif")'
+fire.backgroundPosition='center'
+fire.backgroundSize='cover'
+
+fight.innerText='Fight'
+
 }
